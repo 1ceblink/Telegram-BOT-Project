7a44c0df2f6ad4b8853c2af972382f768f80cd35
Armen <108435601+1ceblink@users.noreply.github.com>
2024-12-14 17:26:43 +0400

    Update User.cs

--- MyTgBot/Model/User.cs
+++ MyTgBot/Model/User.cs
@@ -1,12 +1,11 @@
 ﻿namespace MyTgBot.Model {
     /// <summary>
     /// Exists as a separator information between users.
-    /// Extremely dubious solution.
     /// </summary>
     class User {
         public int OperationLevel { get; set; } = 0;
 
-        // stub, in a real version there will be a call to the DB
+        // stub, in a real project there will be a call to the DB
         public List<NotifierData> userNotifies { get; private set; } = new();
         public int NotifyCount => userNotifies.Count;
 
